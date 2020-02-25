9b2ca9d464d94492aa67a467e421d9c694e83a2c
ben <b.talbot@qut.edu.au>
2020-02-25 18:28:30 +1000

    Move hello_benchbot to simplified & structured API

--- hello_benchbot/line_agent.py
+++ hello_benchbot/line_agent.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from benchbot_api import ActionResult, Agent
+from benchbot_api.tools import ObservationVisualiser
+
 try:
     input = raw_input
 except NameError:
     pass
 
 
-class LineAgent(object):
+class LineAgent(Agent):
     _ACTIONS = [('move_distance', {
         'distance': 1.0
     }), ('move_angle', {
@@ -16,53 +19,48 @@ class LineAgent(object):
 
     _ACTION_SEQUENCE = [0, 1, 1]
 
+    _ITERATION_LIMIT = 10
+
     def __init__(self):
-        self.fig = None
-        self.axs = None
+        self.vis = ObservationVisualiser()
+
         self.next_action_index = 0
+        self.iteration_count = 0
 
-    def __visualise_observations(self, observations):
-        if self.fig is None:
-            plt.ion()
-            self.fig, self.axs = plt.subplots(2, 2)
-            self.fig.canvas.set_window_title("Agent Observations")
-        self.axs[0, 0].clear()
-        self.axs[0, 0].imshow(observations['image_rgb'])
-        self.axs[0, 0].get_xaxis().set_visible(False)
-        self.axs[0, 0].get_yaxis().set_visible(False)
-        self.axs[0, 0].set_title("image_rgb")
-        self.axs[1, 0].clear()
-        self.axs[1, 0].imshow(observations['image_depth'],
-                              cmap="hot",
-                              clim=(np.amin(observations['image_depth']),
-                                    np.amax(observations['image_depth'])))
-        self.axs[1, 0].get_xaxis().set_visible(False)
-        self.axs[1, 0].get_yaxis().set_visible(False)
-        self.axs[1, 0].set_title("image_depth")
-        self.axs[0, 1].clear()
-        self.axs[0, 1].plot(0, 0, c='r', marker=">")
-        self.axs[0, 1].scatter(
-            [x[0] * np.cos(x[1]) for x in observations['laser']['scans']],
-            [x[0] * np.sin(x[1]) for x in observations['laser']['scans']],
-            c='k',
-            s=4,
-            marker='s')
-        self.axs[0, 1].set_title("laser (robot frame)")
-        self.axs[1, 1].clear()
-        # TODO
-        self.axs[1, 1].plot()
-        self.axs[1, 1].set_title("poses (world frame)")
+    def is_done(self, action_result):
+        # Continue as long as we have a action_result of SUCCESS & have not
+        # reached iteration_count
+        return (self.iteration_count >= LineAgent._ITERATION_LIMIT or
+                action_result != ActionResult.SUCCESS)
 
-    def is_done(self):
-        # Go FOREVER
-        return False
+    def pick_action(self, observations, action_list):
+        # Perform a sanity check to confirm we have valid actions available
+        if ('move_distance' not in action_list or
+                'move_angle' not in action_list):
+            raise ValueError(
+                "We don't have any usable actions. Is BenchBot running in the "
+                "right mode (active), or should it have exited (collided / "
+                "finished)?")
 
-    def pick_action(self, observations):
-        self.__visualise_observations(observations)
+        # Update the visualisation
+        self.vis.visualise(
+            observations,
+            self.iteration_count * len(LineAgent._ACTION_SEQUENCE) +
+            self.next_action_index)
+
+        # Wait until the user presses Enter to perform the next action
         input("Press Enter to execute next action")
+
+        # Figure out which action, & return the selection
         action = LineAgent._ACTIONS[LineAgent._ACTION_SEQUENCE[
             self.next_action_index]]
-        self.next_action_index = (
-            0 if self.next_action_index == len(self._ACTION_SEQUENCE) -
-            1 else self.next_action_index + 1)
-        return action
+        if self.next_action_index == len(self._ACTION_SEQUENCE) - 1:
+            self.next_action_index = 0
+            self.iteration_count += 1
+        else:
+            self.next_action_index += 1
+        return (action if action[0] in action_list else (None, {}))
+
+    def save_result(self, filename):
+        # We have no results to save here
+        return
